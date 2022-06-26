314ad529a408e57d84ffc572be8605bb15e61156
Lesha! <8900800700a@gmail.com>
2022-06-26 13:59:12 +0300

    footer fix
    changing the footer tag to semantically correct

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import styled from "styled-components";
 
 const Footer = () =>
 {
     return(
-        <div style={{width:'100%', height:'100px'}}/>
+        <footer style={{width:'100%', height:'100px'}}/>
     )
 }
 
