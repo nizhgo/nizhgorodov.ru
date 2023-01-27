ff5d7282cc92cebc08a16379386526aadbc2605f
Nizhgo <8900800700a@gmail.com>
2023-01-27 15:29:55 +0300

    rename files

--- src/portfolioPage/components/petProjects/PetProjects.tsx
+++ src/portfolioPage/components/petProjects/PetProjects.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import BebraBankGif from '../../../assets/images/bebraBank.gif'
+import BebraBankScreencast from '../../../assets/images/bebraBankScreencast.gif'
 import BebraBankMoc from '../../../assets/images/bebraBankMoc.png'
 import DarkMockup from '../../../assets/images/darkMockup.png'
 import NizhgoCatsMoc from '../../../assets/images/nizhgoCatsMoc.png'
-import OrganizerGif from '../../../assets/images/organizer.webp'
+import NizhgoOrganizerScreencast from '../../../assets/images/nizhgoOrganizerScreencast.webp'
 import WhiteMockup from '../../../assets/images/whiteMockup.png'
-import CipherWebp from '../../../assets/images/cipher.webp'
+import NizhgoCipherScreencast from '../../../assets/images/nizhgoChipherScreencast.webp'
 import PetProjectItem from "./petProjectItem/PetProjectItem";
-import CatsScreencast from '../../../assets/images/cats.webp'
+import NizhgoCatsScreencast from '../../../assets/images/nizhgoCatsScreencast.webp'
 import {
 	PetProjectHeader,
 	PetProjectsContainer,
@@ -23,20 +23,20 @@ const PetProjects = () => {
 			<PetProjectsGrid>
 				<PetProjectItem
 					description={'Таск менеджер. Переключение месяцов. Добавление, редактирование и удаление тасков. Счетчик незаврешенных задач. Задачи сохраняются в local storage. Формы через React Hook Form и валидацией через Yup'}
-					visual={OrganizerGif} mockup={WhiteMockup} title={'ORGANIZER'}
+					visual={NizhgoOrganizerScreencast} mockup={WhiteMockup} title={'ORGANIZER'}
 					siteLink={'https://organizer.nizhgorodov.ru'} githubLink={'https://github.com/nizhgo/organizer'}/>
 				<PetProjectItem
 					description={'Небольшая соцсеть с возможность смотреть случайных котов, авторизацией, лайками и общей лентой лайкнутых кошечек. Самый первый пет-проджект. БД — firebase'}
-					visual={CatsScreencast} mockup={NizhgoCatsMoc} title={'NIZHGOCATS'}
+					visual={NizhgoCatsScreencast} mockup={NizhgoCatsMoc} title={'NIZHGOCATS'}
 					siteLink={'https://cats.nizhgorodov.ru'} githubLink={'https://github.com/nizhgo/nizhgo-cats'}/>
 				<PetProjectItem
 					description={'DES шифрование и дешифрование в формате переводчка. Выбор темы и локализации. Vite + React + Typescript'}
-					visual={CipherWebp} mockup={DarkMockup} title={'CIPHER'}
+					visual={NizhgoCipherScreencast} mockup={DarkMockup} title={'CIPHER'}
 					siteLink={'https://cipher.nizhgorodov.ru/'}
 					githubLink={'https://github.com/nizhgo/cipherCipherCipher'}/>
 				<PetProjectItem
 					description={'Вымышленный финтех стартап. Красивый лэндинг. Регистрация, выбор дизайна карта, переводы и пополенения. Работа с формами через Formik + yup'}
-					visual={BebraBankGif} mockup={BebraBankMoc} title={'BEBRA BANK'} siteLink={'https://bebrabank.ru'}
+					visual={BebraBankScreencast} mockup={BebraBankMoc} title={'BEBRA BANK'} siteLink={'https://bebrabank.ru'}
 					githubLink={'https://github.com/nizhgo/bebra-bank'}/>
 			</PetProjectsGrid>
 		</PetProjectsContainer>
